6631b76869ea969f9d970ece0d5a8ab37c9cf052
K.Mohith Gupta <mohithguptak@gmail.com>
2020-11-16 12:36:55 +0530

    changed all heading tags to p tags

--- index.js
+++ index.js
@@ -84,10 +84,10 @@ new TypeIt("#type2", {
 gsap.from(".logo", { opacity: 0, duration: 1, delay: 0.5, y: -10 });
 gsap.from(".hamburger", { opacity: 0, duration: 1, delay: 1, x: 20 });
 gsap.from(".mypic", { opacity: 0, duration: 1, delay: 1.5, x: -200 });
-gsap.from(".me h3", { opacity: 0, duration: 1, delay: 2, y: -50 });
-gsap.from(".me h1", { opacity: 0, duration: 1, delay: 2.5, y: -45 });
-gsap.from(".me h4", { opacity: 0, duration: 1, delay: 3, y: -30 });
-gsap.from(".me a", { opacity: 0, duration: 1, delay: 3.5, y: 50 });
+gsap.from(".me h3", { opacity: 0, duration: 1, delay: 1.5, y: -50 });
+gsap.from(".me p", { opacity: 0, duration: 1, delay: 2, y: -45 });
+gsap.from(".me h4", { opacity: 0, duration: 1, delay: 2.5, y: -30 });
+gsap.from(".me a", { opacity: 0, duration: 1, delay: 3, y: 50 });
 gsap.from(".nav-item", {  opacity: 0,duration: 1,  delay: 1.2,y:30,  stagger: 0.2,});
 gsap.from(".social-icons a", {   opacity: 0,  duration: 1,  delay: 1.2,  stagger: 0.2,});
 
