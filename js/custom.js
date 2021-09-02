03d5fc7e8fd478071aa9458e98022dc5d65d5e15
You Know Who <you@example.commohithguptak@gmail.com>
2021-09-02 09:55:04 +0530

    Updated The whole Portfolio. Version 2.0git add .

---
+++ js/custom.js
@@ -0,0 +1,54 @@
+const d = new Date();
+document.getElementById("age").innerHTML = d.getFullYear() - 2001 - (d.getMonth() >= 4 ? 0 : 1);
+
+
+new TypeIt("#type2", {
+    speed: 120,
+    loop: true,
+    waitUntilVisible: true,
+})
+
+
+.type("C++", { delay: 400 })
+    .pause(1000)
+    .delete(13)
+    .type("DSA", { delay: 400 })
+    .pause(1000)
+    .delete(10)
+    .type("JavaScript", { delay: 400 })
+    .pause(1000)
+    .delete(13)
+    .type("React", { delay: 400 })
+    .pause(1000)
+    .delete(9)
+    .pause(10)
+    .type("Git", { delay: 400 })
+    .pause(1000)
+    .delete(9)
+    .go();
+
+
+(function($) {
+
+    $(document).ready(function() {
+
+        $(".revealator-delay1").addClass('no-transform');
+
+        function checkSize() {
+            if ($(document).width() > 992) {
+                var btn_hover = "";
+                $(".btn").each(function() {
+                    var btn_text = $(this).text();
+                    $(this).addClass(btn_hover).empty().append("<span data-hover='" + btn_text + "'>" + btn_text + "</span>");
+                });
+            }
+        }
+
+        checkSize();
+
+        window.addEventListener('resize', function() {
+            checkSize();
+        })
+    });
+
+})(jQuery);
