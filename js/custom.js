152455bcf561cc6cae564c783d70d05b3cf35bfd
unknown <mohithguptak@gmail.com>
2023-10-24 16:02:23 +0530

    aded new project and click to copy mail

--- js/custom.js
+++ js/custom.js
@@ -1,32 +1,50 @@
-const d = new Date();
-document.getElementById("age").innerHTML = d.getFullYear() - 2001 - (d.getMonth() >= 4 ? 0 : 1);
-
-
-new TypeIt("#type2", {
-    speed: 120,
-    loop: true,
-    waitUntilVisible: true,
-})
-
-.type("React", { delay: 400 })
-    .pause(1000)
-    .delete(10)
-    .type("JavaScript", { delay: 400 })
-    .pause(1000)
-    .delete(13)
-    .type("Blog", { delay: 400 })
-    .pause(1000)
-    .delete(9)
-    .pause(10)
-    .type("Git", { delay: 400 })
-    .pause(1000)
-    .delete(9)
-    .go();
-
-
 (function($) {
 
+    if(document.getElementById("age")){
+
+        const d = new Date();
+        document.getElementById("age").innerHTML = d.getFullYear() - 2001 - (d.getMonth() >= 4 ? 0 : 1);
+        
+        new TypeIt("#type2", {
+            speed: 150,
+            loop: true,
+            waitUntilVisible: true,
+        })
+
+        .type("React", { delay: 400 })
+            .pause(1000)
+            .delete(10)
+            .type("JavaScript", { delay: 400 })
+            .pause(1000)
+            .delete(13)
+            .type("Blogs", { delay: 400 })
+            .pause(1000)
+            .delete(9)
+            .pause(10)
+            .type("Git", { delay: 400 })
+            .pause(1000)
+            .delete(9)
+            .type("Humor :)", { delay: 400 })
+            .pause(1000)
+            .delete(9)
+            .go();
+    }
+    
     $(document).ready(function() {
+
+        if($(".copy-mail-link")){
+            $(".copy-mail-link").click(function(e){
+                e.preventDefault()
+                navigator.clipboard.writeText('mohithguptak@gmail.com')
+                .then(() => alert("Email copied to clipboard!"))
+                .catch(() => alert('Could not copy the Email'))
+            })
+            let clipboard = new Clipboard('.copy-mail-link');
+
+            clipboard.on('success', () => alert("Email copied to clipboard!"));
+
+            clipboard.on('error', () => alert('Could not copy the Email'));
+        }
 
         $(".revealator-delay1").addClass('no-transform');
 
